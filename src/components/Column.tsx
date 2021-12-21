7d2acec5e06e99d4812b4451f3d227c2c197c415
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-21 15:08:53 +0200

    Added tasklist component

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import TaskCard from './TaskCard';
+import {Droppable} from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import Task from '../interfaces/task';
+import Column from '../interfaces/column';
 
-export default function Column(props: { columnTitle: any; tasks: any; }) {
 
-  const { columnTitle, tasks } = props;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+
+export default function BoardColumn(props: {columnData:Column}) {
+
+  const { columnData } = props;
   return (
 
-      <div className='bg-gray-100 rounded-lg px-3 py-3 column-width mr-4'>
 
-          <p className="text-gray-700 font-semibold font-sans tracking-wide text-sm">{columnTitle}</p>
+              <div className='bg-gray-100 rounded-lg px-3 py-3 column-width mr-4'>
 
-          <TaskCard/>
+      <p className="text-gray-700 font-semibold font-sans tracking-wide text-sm">{columnData.title}</p>
+
+      <Droppable droppableId={columnData.id} >
+        {
+
+          (provided) => (
+            <TaskList
+            ref={provided.innerRef} {...provided.droppableProps}
+            >
+              {columnData.tasks.map((task: Task, index: React.Key | null | undefined) => (
+                <TaskCard key={task.id} index={index} task={task}  />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )
+        }
+
+        </Droppable>
+
+
       </div>
+
   )
+
 }
+
+
+
