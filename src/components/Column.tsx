245ce23ab45c1baf696d2e8a57381da56449ae8d
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-22 14:59:57 +0200

    Added column components

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,53 +1,66 @@
 import React from 'react'
 import TaskCard from './TaskCard';
-import {Droppable} from 'react-beautiful-dnd';
+import {Droppable, Draggable} from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import Task from '../interfaces/task';
 import Column from '../interfaces/column';
 
-
-const TaskList = styled.div`
+const TaskList = styled.div `
   padding: 8px;
   min-width: 320px;
   width: 320px;
   min-height: 100px;
 `;
 
+const Container = styled.div `
+display: flex;
+flex-direction: column;
+`
 
-export default function BoardColumn(props: {columnData:Column}) {
+const Title = styled.h3 `
+  padding: 8px;
+  --tw-text-opacity: 1;
+  color: rgba(64, 64, 64, var(--tw-text-opacity));
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  font-weight: 600;
+  display: inline-block;
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+`;
 
-  const { columnData } = props;
-  return (
+export default function BoardColumn(props : {
+    columnData: Column,
+    index: number
+}) {
+
+    const {columnData, index} = props;
+    return (
+
+        <div>
+                <Container>
+                    <div className='bg-gray-100 rounded-lg px-3 py-3 mr-4'>
+
+                        <Title>{columnData.title}</Title>
+
+                        <Droppable droppableId={columnData.id} type="task">
+                            {(provided) => (
+                                <TaskList
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                    className='min-h-screen'>
+
+                                    {columnData.tasks.map((task : Task, index : React.Key | null | undefined) => (<TaskCard key={task.id} index={index} task={task}/>))}
+                                    {provided.placeholder}
+                                </TaskList>)}
+
+                        </Droppable>
+
+                    </div>
+                </Container>
 
 
-              <div className='bg-gray-100 rounded-lg px-3 py-3 mr-4'>
+        </div>
 
-      <p className="text-gray-700 font-semibold font-sans tracking-wide text-sm">{columnData.title}</p>
-
-      <Droppable droppableId={columnData.id} >
-        {
-
-          (provided) => (
-            <TaskList
-            ref={provided.innerRef} {...provided.droppableProps}
-            className='min-h-screen'
-            >
-              {columnData.tasks.map((task: Task, index: React.Key | null | undefined) => (
-                <TaskCard key={task.id} index={index} task={task}  />
-              ))}
-              {provided.placeholder}
-            </TaskList>
-          )
-        }
-
-        </Droppable>
-
-
-      </div>
-
-  )
+    )
 
 }
-
-
-
