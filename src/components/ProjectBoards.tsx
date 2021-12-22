852d1cff13d145986474a4db1eb28a6739f05719
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-22 15:00:22 +0200

    Added projects boards component

--- src/components/ProjectBoards.tsx
+++ src/components/ProjectBoards.tsx
@@ -1,45 +1,48 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import {DragDropContext} from 'react-beautiful-dnd'
-import { exampleBoard } from '../seeds/boardExample'
+import {exampleBoard} from '../seeds/boardExample'
 import BoardColumn from './Column'
-import {useRecoilState,} from 'recoil';
+import {useRecoilState} from 'recoil';
 import boardState from '../atoms/boardState';
+import styled from 'styled-components';
 import ProjectBoardController from '../controllers/projectBoardController'
+import NewColumn from './board/NewColumn';
 
-
-
-
-
+const Container = styled.div `
+    display: flex;
+  `
 
 export default function ProjectBoards() {
 
-  const [board, setBoard] = useRecoilState(boardState);
+    const [board,
+        setBoard] = useRecoilState(boardState);
 
-  useEffect(() => {
-    setBoard(exampleBoard);
-  }, [])
+    useEffect(() => {
+        setBoard(exampleBoard);
+    }, [])
 
-  let projectBoardCtrller = new ProjectBoardController(board);
+    let projectBoardCtrller = new ProjectBoardController(board);
+
+    const onDragEnd = (result : any) => {
+        let newBoard = projectBoardCtrller.handleOnDragEnd(result);
+        newBoard
+            ? setBoard(newBoard)
+            : null;
+    }
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+
+            <Container>
+                {board.map((column, index) => {
+                    return <BoardColumn key={column.id} index={index} columnData={column}/>
+                })
 
 
-  const onDragEnd = (result:any) => {
-      let newBoard = projectBoardCtrller.handleOnDragEnd(result);
-      newBoard ? setBoard(newBoard) : null;
-  }
+}
 
-
-
-
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      {
-        board.map((column, index) => {
-            return <BoardColumn
-                    key={index}
-                    columnData={column}/>
-        })
-      }
-
-    </DragDropContext>
-  )
+<NewColumn/>
+            </Container>
+        </DragDropContext>
+    )
 }
